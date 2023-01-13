731f9cf9c6973fc0a918953ead252de225780244
Tal <trimoni8@gmail.com>
2023-01-13 10:01:38 -0500

    aliens moving down

--- js/app.js
+++ js/app.js
@@ -74,6 +74,10 @@ function update() {
       alien.x += alienVelocityX
       if (alien.x + alien.width >= board.width || alien.x <= 0){
         alienVelocityX *= -1
+
+        for (let j = 0; j < alienArray.length; j++){
+          alienArray[j].y += alienHeight
+        }
       }
       context.drawImage(alienImg, alien.x, alien.y, alien.width, alien.height)
     }
