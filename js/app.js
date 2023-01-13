39ace1f985afa3933208ba6b135a744f807b6071
Tal <trimoni8@gmail.com>
2023-01-13 10:06:37 -0500

    shooot funct

--- js/app.js
+++ js/app.js
@@ -115,3 +115,15 @@ function createAliens() {
   }
   alienCount = alienArray.length
 }
+
+function shoot(e) {
+  if (e.code == 'Space'){
+    let bullet = {
+      x : ship.x + shipWidth*15/32,
+      y : ship.y,
+      width : tileSize/8,
+      height: tileSize/2,
+      used : false
+    }
+  }
+}
