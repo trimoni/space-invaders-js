a0cca74d790e99ece96df3619f51eba2089073d9
Tal <trimoni8@gmail.com>
2023-01-13 10:15:31 -0500

    detect collision funct

--- js/app.js
+++ js/app.js
@@ -142,3 +142,10 @@ function shoot(e) {
     bulletArray.push(bullet)
   }
 }
+
+function detectCollision(a, b){
+  return a.x < b.x + b.width && 
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+}
